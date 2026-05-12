07dd003a682f2377b05e13194141473f07132358
Xenxo Fernández <xenxo.fernandez@rai.usc.es>
2026-05-12 18:24:45 +0200

    Falta hacer que ejecute un porcentaje del problema

--- problemas/Problema_002/src/Problema_002_esp_1.c
+++ problemas/Problema_002/src/Problema_002_esp_1.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <math.h>
+#include <unistd.h>
 
+#define N_SCHEDS 2
 /**
  * En este código comprobaremos los numeros primos que existe en el rango [A,A+B]
  */
@@ -12,7 +14,11 @@ int main(int argc, char *argv[])
 
     long long A, B;           // Parametros para primos
     int T;                    // Numero de hilos
-    long long num_primos = 0; // Numero de primos entre A y A+B
+    long long num_primos_thr[N_SCHEDS] = {0,0}; // Numero de primos entre A y A+B
+    long long num_primos = 0;
+    volatile char winner = 'X';                  // S -> Static, D -> Dynamic y X -> Por decidir
+    const volatile char *ptr_winner = &(winner); // Usamos un puntero porque si usáramos la variable winner, cada for "cachearía" su valor y no serviría de flag (Por lo menos en mi ordenador)
+
     if (argc < 4)
     {
         printf("ERROR: Uso ./%s <A> <B> <T>\n", argv[0]);
@@ -24,6 +30,8 @@ int main(int argc, char *argv[])
     B = atoll(argv[2]);
     T = atoi(argv[3]);
 
+    omp_set_num_threads(T); // Numero total de hilos disponibles
+    omp_set_nested(1);      // Activamos el paralelismo Aninado
     if (A < 2 || B < 1 || T < 1)
     {
         printf("Argumentos inconsistentes\n");
@@ -31,28 +39,75 @@ int main(int argc, char *argv[])
     }
 
     double start = omp_get_wtime();
-
-#pragma omp parallel for schedule(runtime) reduction(+ : num_primos) num_threads(T)
-    for (long long i = A; i <= A + B; i++)
+#pragma omp parallel sections
     {
-        int isprime = 1; // Todo numero es primo hasta que se demuestre lo contrario
-        for (long long j = 2; j <= sqrt(i); j++)
+#pragma omp section
         {
-            if (i % j == 0)
+            printf("STATIC\n");
+            omp_set_num_threads(T / 2);
+#pragma omp parallel for schedule(static) reduction(+ : num_primos_thr[0]) num_threads(T/2)
+            for (long long i = A; i <= A + B; i++)
             {
-                isprime = 0;
-                break;
+                int isprime = 1; // Todo numero es primo hasta que se demuestre lo contrario
+                for (long long j = 2; j <= sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isprime = 0;
+                        break;
+                    }
+                }
+                if (isprime)
+                {
+                    num_primos_thr[0]++;
+                }
+            }
+            #pragma omp critical
+            {
+                num_primos = num_primos_thr[0];
+                if(*(ptr_winner) == 'X')
+                {
+                    winner = 'S';
+                }
             }
         }
-        if (isprime)
+#pragma omp section
         {
-            num_primos++;
+            printf("DYNAMIC\n");
+            omp_set_num_threads(T / 2);
+#pragma omp parallel for schedule(dynamic) reduction(+ : num_primos_thr[1]) num_threads(T/2)
+            for (long long i = A; i <= A + B; i++)
+            {
+                int isprime = 1; // Todo numero es primo hasta que se demuestre lo contrario
+                for (long long j = 2; j <= sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isprime = 0;
+                        break;
+                    }
+                }
+                if (isprime)
+                {
+                    num_primos_thr[1]++;
+                }
+            }
+            #pragma omp critical
+            {
+                num_primos = num_primos_thr[1];
+                if(*(ptr_winner) == 'X')
+                {
+                    winner = 'D';
+                }
+            }
         }
     }
+
     double end = omp_get_wtime();
 
-    //printf("Num de primos encontrados: %lld\n", num_primos);
+    printf("Num de primos encontrados: %lld\n", num_primos);
     printf("TIME: %lf\n", end - start);
+    printf("WINNER: %c\n", winner);
 
     return 0;
 }
