9d32793322fbc6dc8f252892a488ea71f60e74c3
Xenxo Fernández <xenxo.fernandez@rai.usc.es>
2026-05-21 17:54:02 +0200

    Modificado en Términos de N

--- problemas/Problema_002/src/Problema_002_esp_1.c
+++ problemas/Problema_002/src/Problema_002_esp_1.c
@@ -12,27 +12,26 @@
 int main(int argc, char *argv[])
 {
 
-    long long A, B;           // Parametros para primos
+    long long N;           // Parametros para primos
     int T;                    // Numero de hilos
     long long num_primos_thr[N_SCHEDS] = {0,0}; // Numero de primos entre A y A+B
     long long num_primos = 0;
     volatile char winner = 'X';                  // S -> Static, D -> Dynamic y X -> Por decidir
     const volatile char *ptr_winner = &(winner); // Usamos un puntero porque si usáramos la variable winner, cada for "cachearía" su valor y no serviría de flag (Por lo menos en mi ordenador)
 
-    if (argc < 4)
+    if (argc < 3)
     {
-        printf("ERROR: Uso ./%s <A> <B> <T>\n", argv[0]);
+        printf("ERROR: Uso ./%s <N> <T>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     // Parseo de Argumentos
-    A = atoll(argv[1]);
-    B = atoll(argv[2]);
-    T = atoi(argv[3]);
+    N = atoll(argv[1]);
+    T = atoi(argv[2]);
 
     omp_set_num_threads(T); // Numero total de hilos disponibles
     omp_set_nested(1);      // Activamos el paralelismo Aninado
-    if (A < 2 || B < 1 || T < 1)
+    if (N < 2 || T < 1)
     {
         printf("Argumentos inconsistentes\n");
         exit(EXIT_FAILURE);
@@ -43,10 +42,9 @@ int main(int argc, char *argv[])
     {
 #pragma omp section
         {
-            printf("STATIC\n");
             omp_set_num_threads(T / 2);
 #pragma omp parallel for schedule(static) reduction(+ : num_primos_thr[0]) num_threads(T/2)
-            for (long long i = A; i <= A + B; i++)
+            for (long long i = 2; i <= N; i++)
             {
                 int isprime = 1; // Todo numero es primo hasta que se demuestre lo contrario
                 for (long long j = 2; j <= sqrt(i); j++)
@@ -73,10 +71,9 @@ int main(int argc, char *argv[])
         }
 #pragma omp section
         {
-            printf("DYNAMIC\n");
             omp_set_num_threads(T / 2);
 #pragma omp parallel for schedule(dynamic) reduction(+ : num_primos_thr[1]) num_threads(T/2)
-            for (long long i = A; i <= A + B; i++)
+            for (long long i = 2; i <= N; i++)
             {
                 int isprime = 1; // Todo numero es primo hasta que se demuestre lo contrario
                 for (long long j = 2; j <= sqrt(i); j++)
