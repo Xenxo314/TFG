c017791f302f4678e56c7a6f7360e0f3de704f4a
Xenxo Fernández <xenxo.fernandez@rai.usc.es>
2026-05-22 11:41:26 +0200

    Modificado para que contemple todas las métricas TEMPORALES faltan las de memoria.

--- problemas/Problema_001/src/Problema_001_esp_2.c
+++ problemas/Problema_001/src/Problema_001_esp_2.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
+#include <omp.h>
 
-double calc_std_dev(double *data, long N)
+double calc_std_dev(double *data, long N, int *T, double *std_perc)
 {
     double sum = 0;
-    for(int i = 0; i < N; i++)
+#pragma omp parallel for reduction(+ : sum) schedule(static) num_threads(*T)
+    for (int i = 0; i < N; i++)
     {
         sum += data[i];
     }
@@ -15,61 +16,116 @@ double calc_std_dev(double *data, long N)
 
     double values = 0;
 
+#pragma omp parallel for reduction(+ : values) schedule(static) num_threads(*T)
     for (long i = 0; i < N; i++)
     {
-        values += (data[i] - media)*(data[i] - media);
+        values += (data[i] - media) * (data[i] - media);
     }
-    
-    double std_dev = sqrt(values/N);
+
+    double std_dev = sqrt(values / N);
+
+    *std_perc = std_dev / media * 100;
 
     return std_dev;
 }
 
 // Programa que calcula PI como la raiz cuadrada de 6 veces la suma de inversos de los N-esimos primeros naturales.
 // sqrt(6*Σ(1/n^2))
+
 int main(int argc, char **argv)
 {
-    // Variables que miden el tiempo
-    clock_t start, end, start_i, end_i;
-    double cpu_time_used;
+    // METRICAS DE TIEMPO
+    double time_tot;             // tiempo total de ejecución (Sin reserva de memoria)
+    double time_dec;             // Tiempo de decisión
+    double start, end;           // Marcadores del tiempo total
+    double end_d;                // Marcador del tiempo de decisión
+    double start_i, end_i;       // Marcadores de inicio de un bloque de iteraciones
+    double std_dev, desbalanceo; // Métricas de desviación
 
-    long N; // Tamaño del Problema
+    // ARGUMENTOS
+    long long N;        // Tamaño del problema
+    int T;              // Numero de hilos
+    double p;           // Porcentaje de la decisión
+    double umbral = 25; // Umbral para decidir si planificación estática o dinámica [OPT]
+    long ckunk = 1;    // Distancia entre iteraciones (De esta manera evitamos errores de medición) [OPT]
+
+    // OTRAS variables
+    long long N_d;     // Tamaño de la decisión
+    char winner = 'X'; // Indica la planificación ganadora
+
+    // Variables de cómputo
     double estimated_pi = 0;
 
     // Comprobación de Parámetros
-    if (argc < 2)
+    if (argc < 4)
     {
-        perror("ERROR: No hay suficientes paramámetros introducidos\n");
+        printf("ERROR: Uso ./%s <N> <T> <p> [umbral] [chunk]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    N = atol(argv[1]);
-    double *times = malloc( N * sizeof(double));
+    // ENTRADA de parametros
+    N = atoll(argv[1]);
+    T = atoi(argv[2]);
+    p = atof(argv[3]);
 
-
-    start = clock();
-    // Hacemos la suma de los inversos al cuadrado
-    for (long long i = 1; i < N; i++)
+    if (argc == 6)
     {
-        start_i = clock();
-        estimated_pi += 1 / (i * (double)i);
-        end_i = clock();
-        times[i - 1] = ((double)(end_i-start_i)) / CLOCKS_PER_SEC;
+        umbral = atoi(argv[4]);
+        ckunk = atol(argv[5]);
     }
 
-    // Multiplicamos y Hacemos la raiz
+    N_d = N * p / 100.0f;
+
+    // RESERVA de memoria
+    double *times = malloc(N_d / ckunk * sizeof(double));
+
+    // COMIENZA EL TIEMPO DE DECISIÓN
+    start = omp_get_wtime();
+
+    for (long long i = 1; i < N_d; i++)
+    {
+        start_i = omp_get_wtime();
+        estimated_pi += 1 / (i * (double)i);
+        if (i % ckunk == 0)
+        {
+            end_i = omp_get_wtime();
+            times[i / ckunk - 1] = end_i - start_i;
+        }
+    }
+
+    // CALCULO MÉTRICAS
+    std_dev = calc_std_dev(times, N_d / ckunk, &T, &desbalanceo);
+
+    // Ajustamos el scheduler
+    (desbalanceo < umbral) ? (omp_set_schedule(omp_sched_static, ckunk), winner = 'S') : (omp_set_schedule(omp_sched_dynamic, ckunk), winner = 'D');
+
+    // FIN TIEMPO DE DECISIÓN
+    end_d = omp_get_wtime();
+
+    // COMPUTO PARALELO
+#pragma omp parallel for reduction(+ : estimated_pi) schedule(runtime) num_threads(T)
+    for (long long i = N_d; i < N; i++)
+    {
+        estimated_pi += 1 / (i * (double)i);
+    }
     estimated_pi *= 6;
     estimated_pi = sqrt(estimated_pi);
 
-    // Paramos Contador
-    end = clock();
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    // FIN TIEMPO TOTAL
+    end = omp_get_wtime();
 
-    printf("TIME: %lf\n", cpu_time_used);
+    time_dec = end_d - start;
+    time_tot = end - start;
 
-    // Calculamos la desviación estandar de los tiempos de cada iteración.
-    double std_dev = calc_std_dev(times,N);
-    printf("STD_DEV = %.15lf\n",std_dev);
+    // PRINTS
+    printf("TIME_TOT: %lf\n", time_tot);
+    printf("TIME_DEC: %lf\n", time_dec);
+    printf("STD_DEV = %.15lf\n", std_dev);
+    printf("DESBALANCEO = %.15lf %%\n", desbalanceo);
+    printf("WINNER = %c\n", winner);
+
+    // FREE
+    free(times);
 
     return 0;
 }
