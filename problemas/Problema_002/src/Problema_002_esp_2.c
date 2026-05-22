c86c216115eabadf8baba844a1b9e58fd2a5e2c3
Xenxo Fernández <xenxo.fernandez@rai.usc.es>
2026-05-22 14:23:54 +0200

    COMPLETO

--- problemas/Problema_002/src/Problema_002_esp_2.c
+++ problemas/Problema_002/src/Problema_002_esp_2.c
@@ -1,62 +1,143 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
+#include <omp.h>
+#include <sys/resource.h>
 
-double calc_std_dev(double *data, long N)
+#define TOLERANCIA 1
+#define EPSILON 1e-7
+
+double calc_std_dev(double *data, long N, int *T, double *std_perc)
 {
     double sum = 0;
-    for(int i = 0; i < N; i++)
+#pragma omp parallel for reduction(+ : sum) schedule(static) num_threads(*T)
+    for (long long i = 0; i < N; i++)
     {
         sum += data[i];
     }
 
     double media = sum / N;
-
     double values = 0;
 
-    for (long i = 0; i < N; i++)
+#pragma omp parallel for reduction(+ : values) schedule(static) num_threads(*T)
+    for (long long i = 0; i < N; i++)
     {
-        values += (data[i] - media)*(data[i] - media);
+        values += (data[i] - media) * (data[i] - media);
     }
-    
-    double std_dev = sqrt(values/N);
+
+    double std_dev = sqrt(values / N);
+
+    *std_perc = std_dev / media * 100;
 
     return std_dev;
 }
 
-// Programa para el cómpputo de números primos
+// Programa que calcula PI como la raiz cuadrada de 6 veces la suma de inversos de los N-esimos primeros naturales.
+// sqrt(6*Σ(1/n^2))
+
 int main(int argc, char **argv)
 {
-    // Variables que miden el tiempo
-    clock_t start, end, start_i, end_i;
-    double cpu_time_used;
+    // METRICAS DE TIEMPO
+    double time_tot;             // tiempo total de ejecución (Sin reserva de memoria)
+    double time_dec;             // Tiempo de decisión
+    double start, end;           // Marcadores del tiempo total
+    double end_d;                // Marcador del tiempo de decisión
+    double start_i, end_i;       // Marcadores de inicio de un bloque de iteraciones
+    double std_dev, desbalanceo; // Desviación típica y coeficiente de variación
 
-    long N; // Tamaño del Problema
-    long num_primos = 0;
+    // ARGUMENTOS
+    long long N;        // Tamaño del problema
+    int T;              // Numero de hilos
+    double p;           // Porcentaje de la decisión
+    double umbral = 25; // Umbral para decidir si planificación estática o dinámica [OPT]
+    long chunk = 1;     // Distancia entre iteraciones (De esta manera evitamos errores de medición) [OPT]
 
-    if (argc < 2)
+    // MEMORIA
+    struct rusage usage;// Tamaño de RSS
+
+    // OTRAS variables
+    long long N_d;     // Tamaño de la decisión
+    char winner = 'X'; // Indica la planificación ganadora
+
+    // Variables de cómputo
+    long long num_primos = 0;
+
+    // Comprobación de Parámetros
+    if (argc < 4)
     {
-        printf("ERROR: Uso ./%s <N>\n", argv[0]);
+        printf("ERROR: Uso ./%s <N> <T> <p> [umbral] [chunk]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
+    // ENTRADA de parametros
     N = atoll(argv[1]);
+    T = atoi(argv[2]);
+    p = atof(argv[3]);
 
-    
-    if (N < 2)
+    if (argc == 6)
     {
-        printf("Argumentos inconsistentes\n");
-        exit(EXIT_FAILURE);
+        umbral = atoi(argv[4]);
+        chunk = atol(argv[5]);
     }
 
-    double *times = malloc(N * sizeof(double));
+    N_d = N * p / 100.0f;
 
-    start = clock();
+    // RESERVA de memoria
+    double *times = malloc(N_d / chunk * sizeof(double));
 
-    for (long long i = 2; i <= N; i++)
+    // COMIENZA EL TIEMPO DE DECISIÓN
+    start = omp_get_wtime();
+
+    for (long long i = 2; i < N_d; i++)
     {
-        start_i = clock();
+        start_i = omp_get_wtime();
+
+        int isprime = 1;
+        int contador = 1;
+    repeat:
+        for (long long j = 2; j <= sqrt(i); j++)
+        {
+            if (i % j == 0)
+            {
+                isprime = 0;
+                break;
+            }
+        }
+        if (isprime)
+        {
+            if (contador == 1)
+            {
+                num_primos++;
+            }
+        }
+        if (i % chunk == 0 || i == (N_d - 1))
+        {
+            end_i = omp_get_wtime();
+            if (end_i - start_i > EPSILON)
+            {
+                times[i / chunk - 1] = (end_i - start_i) / contador;
+            }
+            else
+            {
+                contador++;
+                goto repeat;
+            }
+        }
+    }
+
+    // CALCULO MÉTRICAS
+    std_dev = calc_std_dev(times, N_d / chunk, &T, &desbalanceo);
+
+    // Ajustamos el scheduler
+    (desbalanceo < umbral) ? (omp_set_schedule(omp_sched_static, chunk), winner = 'S') : (omp_set_schedule(omp_sched_dynamic, chunk), winner = 'D');
+
+    // FIN TIEMPO DE DECISIÓN
+    end_d = omp_get_wtime();
+
+    // COMPUTO PARALELO
+#pragma omp parallel for reduction(+ : num_primos) schedule(runtime) num_threads(T)
+    for (long long i = N_d; i <= N; i++)
+    {
         int isprime = 1;
         for (long long j = 2; j <= sqrt(i); j++)
         {
@@ -64,27 +145,35 @@ int main(int argc, char **argv)
             {
                 isprime = 0;
                 break;
-
             }
         }
         if (isprime)
         {
             num_primos++;
         }
-        times[i - 1] = ((double)(end_i-start_i)) / CLOCKS_PER_SEC;
     }
 
+    // FIN TIEMPO TOTAL
+    end = omp_get_wtime();
 
-    // Paramos Contador
-    end = clock();
-    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    time_dec = end_d - start;
+    time_tot = end - start;
 
-    printf("TIME: %lf\n", cpu_time_used);
+    // Medir memoria
+    getrusage(RUSAGE_SELF, &usage);
 
-    // Calculamos la desviación estandar de los tiempos de cada iteración.
-    double std_dev = calc_std_dev(times,N);
-    printf("STD_DEV = %.15lf\n",std_dev);
-    printf("100_DEV = %.15lf\n",std_dev/cpu_time_used * 100);
+    // PRINTS
+    printf("TIME_TOT: %lf\n", time_tot);
+    printf("TIME_DEC: %lf\n", time_dec);
+    printf("STD_DEV = %.15lf\n", std_dev);
+    printf("DESBALANCEO = %.15lf %%\n", desbalanceo);
+    printf("WINNER = %c\n", winner);
+    printf("Num primos = %lld\n", num_primos);
+
+    printf("Max RSS: %ld KB\n", usage.ru_maxrss);
+
+    // FREE
+    free(times);
 
     return 0;
 }
