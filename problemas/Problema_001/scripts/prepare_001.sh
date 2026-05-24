1ae8d452c613e809275b9a8099d7fdce3aa49093
Xenxo Fernández <xenxo.fernandez@rai.usc.es>
2026-05-24 20:08:21 +0200

    Modificado el nombre de archivos y utilizada la opción -O0

--- problemas/Problema_001/scripts/prepare_001.sh
+++ problemas/Problema_001/scripts/prepare_001.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compilamos los archivos del código fuente
-gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/sequential/Problema_001_secuencial.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/execs/P_001_seq -lm
-gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/openmp/Problema_001_omp.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/execs/P_001_omp -lm -fopenmp
-gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/especulativo/Problema_001_especulativo_1.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/execs/P_001_esp_1 -lm -fopenmp
-gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/especulativo/Problema_001_especulativo_2.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/execs/P_001_esp_2 -lm -fopenmp
+gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/sequential/Problema_001_sec.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/execs/P_001_seq -lm -O0
+gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/openmp/Problema_001_omp.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/execs/P_001_omp -lm -fopenmp -O0
+gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/especulativo/Problema_001_esp_1.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/execs/P_001_esp_1 -lm -fopenmp -O0
+gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/especulativo/Problema_001_esp_2.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_001/execs/P_001_esp_2 -lm -O0
 
 # Inicializamos las variables de entorno (CANCELLATION)
 export OMP_CANCELLATION=true
