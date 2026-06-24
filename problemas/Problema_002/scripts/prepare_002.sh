a80f08401908e1ac3951f32865eb8d7abc6652e8
Xenxo Fernández <xenxo.fernandez@rai.usc.es>
2026-06-24 17:51:04 +0200

    Added Dynamic Path en EXP_2

--- problemas/Problema_002/scripts/prepare_002.sh
+++ problemas/Problema_002/scripts/prepare_002.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Compilamos los archivos del código fuente
-gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_002/src/Problema_002_sec.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_002/execs/P_002_seq -lm -O0
-gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_002/src/Problema_002_omp.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_002/execs/P_002_omp -lm -fopenmp -O0
-gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_002/src/Problema_002_esp_1.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_002/execs/P_002_esp_1 -lm -fopenmp -O0
-gcc /home/usc/cursos/curso1285/home/TFG/problemas/Problema_002/src/Problema_002_esp_2.c -o /home/usc/cursos/curso1285/home/TFG/problemas/Problema_002/execs/P_002_esp_2 -lm -O0 -fopenmp
+BASE_DIR=$1
 
-# Inicializamos las variables de entorno (CANCELLATION)
+gcc "$BASE_DIR/problemas/Problema_002/src/Problema_002_sec.c" -o "$BASE_DIR/problemas/Problema_002/execs/P_002_seq" -lm -O0
+gcc "$BASE_DIR/problemas/Problema_002/src/Problema_002_omp.c" -o "$BASE_DIR/problemas/Problema_002/execs/P_002_omp" -lm -fopenmp -O0
+gcc "$BASE_DIR/problemas/Problema_002/src/Problema_002_esp_1.c" -o "$BASE_DIR/problemas/Problema_002/execs/P_002_esp_1" -lm -fopenmp -O0
+gcc "$BASE_DIR/problemas/Problema_002/src/Problema_002_esp_2.c" -o "$BASE_DIR/problemas/Problema_002/execs/P_002_esp_2" -lm -O0 -fopenmp
+
+
+# Inicializamos variables de entorno
 export OMP_CANCELLATION=true
-
-
